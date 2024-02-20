d77dd58b5dbfa581b6936bf5352e82977c10fc5f
Rits113116 <rr4618634@gmail.com>
2024-02-21 01:49:07 +0530

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,11 @@ import { FaEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import {toast} from 'react-toastify'
+import { useNavigate } from "react-router-dom";
 export default function SignIn() {
+  const navigate=useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -15,6 +19,20 @@ export default function SignIn() {
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  }
+  async function onSubmit(e)
+  {
+    e.preventDefault();
+    try {
+      const auth=getAuth()
+      const userCredentials=await signInWithEmailAndPassword(auth,email,password)
+      if(userCredentials.user)
+      {
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
   }
   return (
     <section>
@@ -28,7 +46,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
